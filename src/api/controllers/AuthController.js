f5cca476ace9d2ae9f69f489fb41ddd282e8cbfe
kikilove5me <77271656+kikilove5me@users.noreply.github.com>
2022-10-24 08:08:24 +0200

    validation on Registration added

--- src/api/controllers/AuthController.js
+++ src/api/controllers/AuthController.js
@@ -1,9 +1,30 @@
+import Role from '../models/Role.js';
+import User from '../models/User.js';
+import Bcrypt from 'bcryptjs';
+import { validationResult } from 'express-validator';
+
 class AuthController {
     async registration(req, res) {
         try {
+            const errors = validationResult(req);
+            if (!errors.isEmpty()) {
+                return res.status(400).json({ message: "error occured during the Registration...", errors })
 
+            }
+            const { username, password } = req.body;
+            const candidate = await User.findOne({ username });
+            if (candidate) {
+                return res.status(400).json("user alrady exist...");
+            }
+
+            const hashPassword = Bcrypt.hashSync(password, 7);
+            const userRole = await Role.findOne({ value: 'USER' });
+            const user = new User({ username, password: hashPassword, roles: [userRole.value] });
+            await user.save();
+            return res.json(user);
         } catch (e) {
-
+            console.log(e);
+            res.status(400).json({ message: ' Registration Error...' });
         }
     }
 
@@ -11,15 +32,21 @@ class AuthController {
         try {
 
         } catch (e) {
-
+            console.log(e);
+            res.status(400).json({ message: 'Login Error...' });
         }
     }
 
     async getUsers(req, res) {
         try {
+            const userRole = new Role();
+            const adminRole = new Role({ value: 'ADMIN' });
+            await userRole.save();
+            await adminRole.save();
             res.json('server works');
         } catch (e) {
-
+            console.log(e);
+            res.status(400).json({ message: 'Error' });
         }
     }
 }
--- src/api/routes/authRouter.js
+++ src/api/routes/authRouter.js
@@ -1,8 +1,19 @@
 import { Router } from 'express';
-import AuthController from '../controllers/AuthController.js'
+import AuthController from '../controllers/AuthController.js';
+import { check } from 'express-validator';
 const router = new Router();
 
-router.post('/registration', AuthController.registration);
+
+router.post(
+    '/registration',
+    [
+        check('username', 'username already exist...').notEmpty(),
+        check('password', 'invalid Password...').isLength({ min: 4, max: 10 })
+    ],
+    AuthController.registration
+);
+
+
 router.post('/login', AuthController.login);
 router.get('/users', AuthController.getUsers);
 
